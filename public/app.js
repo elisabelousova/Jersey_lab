6fecb60911fac7be980d221aacf28e8395f4b955
elisabelousova <elizabethbelousova@gmail.com>
2026-05-27 14:07:54 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
 const ADMIN_USERNAME = 'jersey_lab_admin';
-const ADMIN_USERNAME = 'elisabelousova';
 const API_URL = 'https://zlgxnrgnpfnjyiugdacu.supabase.co/functions/v1/products-api';
 const CHANNEL_URL = 'https://t.me/jersey_lab';
 
